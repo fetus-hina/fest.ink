58b214efd1e4b151c583dc2464dcd8108f040ab1
AIZAWA, Hina <hina@bouhime.com>
2015-09-02 22:28:51 +0900

    ライセンス表記微調整

--- resources/fest.ink/fest.js/00-copyright.js
+++ resources/fest.ink/fest.js/00-copyright.js
@@ -1,1 +1,5 @@
-/*! Copyright (C) 2015 AIZAWA Hina | MIT License */
+/*! Copyright (C) 2015 AIZAWA Hina | MIT License
+ *
+ * Array.prototype.indexOf implementation was copied from:
+ *   https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+ */
