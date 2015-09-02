080b806dd7f9b455ad83069a7e8f84ecff7c6bd0
AIZAWA, Hina <hina@bouhime.com>
2015-09-02 22:34:22 +0900

    jQuery 2.1 を使っている時点で IE9+ が対象なので IE9+ で対応している [].indexOf は何もしなくても使えるはず

--- resources/fest.ink/fest.js/00-copyright.js
+++ resources/fest.ink/fest.js/00-copyright.js
@@ -1,5 +1,1 @@
-/*! Copyright (C) 2015 AIZAWA Hina | MIT License
- *
- * Array.prototype.indexOf implementation was copied from:
- *   https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
- */
+/*! Copyright (C) 2015 AIZAWA Hina | MIT License */
