b3fe2bfe3a94450a7779c41c33666264d8644557
AIZAWA, Hina <hina@bouhime.com>
2015-09-13 21:23:33 +0900

    APIに日時の文字列表現を追加,状態の表現を明確な文字列に変更

--- actions/fest/IndexJsonAction.php
+++ actions/fest/IndexJsonAction.php
@@ -7,19 +7,43 @@
 
 namespace app\actions\fest;
 
+use DateTime;
+use DateTimeZone;
 use Yii;
 use yii\web\ViewAction as BaseAction;
 use app\components\Version;
 use app\models\Fest;
+use app\models\Timezone;
 
 class IndexJsonAction extends BaseAction
 {
     public function run()
     {
+        $request = Yii::$app->getRequest();
+        $tz = $request->get('tz');
+        if (!is_scalar($tz) || !Timezone::findOne(['zone' => $tz])) {
+            $tz = Yii::$app->timeZone;
+        }
+
+        $time2str = function ($time) use ($tz) {
+            $t1 = (int)floor((float)$time); // time の整数部
+            $t2 = (float)$time - $t1;       // time の小数部
+
+            $dateTime = DateTime::createFromFormat(
+                'U u',
+                sprintf('%d %06d', $t1, (int)floor($t2 * 1000000))
+            );
+            $dateTime->setTimeZone(new DateTimeZone($tz));
+            return $t2 > 0
+                ? $dateTime->format('Y-m-d\TH:i:s.uP')
+                : $dateTime->format('Y-m-d\TH:i:sP');
+        };
+
         $now = isset($_SERVER['REQUEST_TIME_FLOAT']) ? $_SERVER['REQUEST_TIME_FLOAT'] : microtime(true);
         Yii::$app->getResponse()->format = 'json';
         return [
             'now' => $now,
+            'now_s' => $time2str($now),
             'source' => [
                 'name'      => Yii::$app->name,
                 'url'       => Yii::$app->getUrlManager()->createAbsoluteUrl(['/fest/index']),
@@ -30,16 +54,26 @@ class IndexJsonAction extends BaseAction
                 ],
             ],
             'fests' => array_map(
-                function ($fest) use ($now) {
+                function ($fest) use ($now, $time2str) {
                     $alpha = $fest->alphaTeam;
                     $bravo = $fest->bravoTeam;
+                    if ((int)$fest->start_at > $now) {
+                        $state = 'scheduled';
+                    } elseif ((int)$fest->end_at > $now) {
+                        $state = 'in session';
+                    } else {
+                        $state = 'closed';
+                    }
                     return [
                         'id'    => (int)$fest->id,
                         'name'  => $fest->name,
                         'term'  => [
                             'begin' => (int)$fest->start_at,
                             'end'   => (int)$fest->end_at,
-                            'in_session' => ((int)$fest->start_at <= $now && $now < (int)$fest->end_at),
+                            'begin_s' => $time2str((int)$fest->start_at),
+                            'end_s'   => $time2str((int)$fest->end_at),
+                            'in_session' => ($state === 'in session'),
+                            'status' => $state,
                         ],
                         'teams' => [
                             'alpha' => [
--- actions/fest/ViewJsonAction.php
+++ actions/fest/ViewJsonAction.php
@@ -7,11 +7,14 @@
 
 namespace app\actions\fest;
 
+use DateTime;
+use DateTimeZone;
 use Yii;
 use yii\web\ViewAction as BaseAction;
 use yii\web\NotFoundHttpException;
 use app\models\Fest;
 use app\models\OfficialData;
+use app\models\Timezone;
 
 class ViewJsonAction extends BaseAction
 {
@@ -23,19 +26,48 @@ class ViewJsonAction extends BaseAction
                 !($fest = Fest::findOne(['id' => $id]))) {
             throw new NotFoundHttpException();
         }
+        $tz = $request->get('tz');
+        if (!is_scalar($tz) || !Timezone::findOne(['zone' => $tz])) {
+            $tz = Yii::$app->timeZone;
+        }
+
+        $time2str = function ($time) use ($tz) {
+            $t1 = (int)floor((float)$time); // time の整数部
+            $t2 = (float)$time - $t1;       // time の小数部
+
+            $dateTime = DateTime::createFromFormat(
+                'U u',
+                sprintf('%d %06d', $t1, (int)floor($t2 * 1000000))
+            );
+            $dateTime->setTimeZone(new DateTimeZone($tz));
+            return $t2 > 0
+                ? $dateTime->format('Y-m-d\TH:i:s.uP')
+                : $dateTime->format('Y-m-d\TH:i:sP');
+        };
 
         Yii::$app->getResponse()->format = 'json';
         $now = isset($_SERVER['REQUEST_TIME_FLOAT']) ? $_SERVER['REQUEST_TIME_FLOAT'] : microtime(true);
+        if ((int)$fest->start_at > $now) {
+            $state = 'scheduled';
+        } elseif ((int)$fest->end_at > $now) {
+            $state = 'in session';
+        } else {
+            $state = 'closed';
+        }
         $alpha = $fest->alphaTeam;
         $bravo = $fest->bravoTeam;
         return [
             'now'   => $now,
+            'now_s' => $time2str($now),
             'id'    => $fest->id,
             'name'  => $fest->name,
             'term'  => [
                 'begin' => $fest->start_at,
                 'end'   => $fest->end_at,
-                'in_session' => ((int)$fest->start_at <= $now && $now < (int)$fest->end_at),
+                'begin_s' => $time2str($fest->start_at),
+                'end_s'   => $time2str($fest->end_at),
+                'in_session' => ($state === 'in session'),
+                'status' => $state,
             ],
             'teams'  => [
                 'alpha' => [
@@ -48,11 +80,12 @@ class ViewJsonAction extends BaseAction
                 ],
             ],
             'wins'   => array_map(
-                function (OfficialData $data) {
+                function (OfficialData $data) use ($time2str) {
                     $alpha = $data->alpha;
                     $bravo = $data->bravo;
                     return [
                         'at'    => $data->downloaded_at,
+                        'at_s'  => $time2str($data->downloaded_at),
                         'alpha' => $alpha ? $alpha->count : 0,
                         'bravo' => $bravo ? $bravo->count : 0,
                     ];
