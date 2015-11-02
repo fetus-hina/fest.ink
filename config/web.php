a99df2d2fd50d2fcb529e67c312e38385c5b0e49
AIZAWA, Hina <hina@bouhime.com>
2015-11-02 20:04:48 +0900

    bump version

--- config/web.php
+++ config/web.php
@@ -2,7 +2,7 @@
 $params = require(__DIR__ . '/params.php');
 $config = [
     'name' => 'イカフェスレート',
-    'version' => '2.0.1',
+    'version' => '2.0.2',
     'id' => 'basic',
     'language' => 'ja-jp',
     'timeZone' => 'Asia/Tokyo',
