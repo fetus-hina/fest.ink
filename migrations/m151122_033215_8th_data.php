52f639667d1eec365e21b8005bdc43744c2f9bd2
AIZAWA, Hina <hina@bouhime.com>
2015-11-22 12:35:26 +0900

    第8回フェスデータ

---
+++ migrations/m151122_033215_8th_data.php
@@ -0,0 +1,12 @@
+<?php
+if (!class_exists('m151024_070218_6th_data', false)) {
+    require_once(__DIR__ . '/m151024_070218_6th_data.php');
+}
+
+class m151122_033215_8th_data extends m151024_070218_6th_data
+{
+    public function getFestId()
+    {
+        return 8;
+    }
+}
