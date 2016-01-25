1492c9182639d0ceb6e4049a5fbea6e96345f215
AIZAWA Hina <hina@bouhime.com>
2016-01-25 22:47:19 +0900

    第10回フェスの公式結果
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m160125_135026_10th_data.php
@@ -0,0 +1,12 @@
+<?php
+if (!class_exists('m151024_070218_6th_data', false)) {
+    require_once(__DIR__ . '/m151024_070218_6th_data.php');
+}
+
+class m160125_135026_10th_data extends m151024_070218_6th_data
+{
+    public function getFestId()
+    {
+        return 10;
+    }
+}
