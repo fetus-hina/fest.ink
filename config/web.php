1dac623cd11f80359c2758ab286b8884f89b714c
AIZAWA Hina <hina@bouhime.com>
2016-07-22 05:09:57 +0900

    v2.7.0
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

--- config/web.php
+++ config/web.php
@@ -2,7 +2,7 @@
 $params = require(__DIR__ . '/params.php');
 $config = [
     'name' => 'イカフェスレート',
-    'version' => '2.7.0-beta3',
+    'version' => '2.7.0',
     'id' => 'basic',
     'language' => 'ja-jp',
     'timeZone' => 'Asia/Tokyo',
