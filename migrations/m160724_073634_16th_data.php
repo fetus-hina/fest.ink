00d0ee699d2b8325f1e8be1c6a5d938efeebc1b4
AIZAWA Hina <hina@bouhime.com>
2016-07-24 16:37:52 +0900

    第16回フェスJSON
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m160724_073634_16th_data.php
@@ -0,0 +1,12 @@
+<?php
+if (!class_exists('m151024_070218_6th_data', false)) {
+    require_once(__DIR__ . '/m151024_070218_6th_data.php');
+}
+
+class m160724_073634_16th_data extends m151024_070218_6th_data
+{
+    public function getFestId()
+    {
+        return 16;
+    }
+}
