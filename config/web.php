119f47266c3aab61aab3c520b005d7d79a15698f
AIZAWA Hina <hina@fetus.jp>
2022-01-13 15:38:17 +0900

    v2.8.15

--- config/web.php
+++ config/web.php
@@ -19,7 +19,7 @@ return (function (): array {
     $params = require(__DIR__ . '/params.php');
     $config = [
         'name' => 'イカフェスレート',
-        'version' => '2.8.13',
+        'version' => '2.8.15',
         'id' => 'basic',
         'language' => 'ja-jp',
         'timeZone' => 'Asia/Tokyo',
